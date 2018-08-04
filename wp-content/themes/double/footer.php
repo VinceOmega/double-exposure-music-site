974ad782c6e4d18f51a339c042aaa74600d03bba
Larry Stanfield <vince.omega@gmail.com>
2018-08-04 18:29:00 -0400

    Added the second triangle, going to add fonts from other computer.

--- wp-content/themes/double/footer.php
+++ wp-content/themes/double/footer.php
@@ -2,8 +2,10 @@
 			
 				<div class="footer__music">
 					
-	           		
-				
+					<i class=""></i>
+	           		<h4 class="footer__music__header">MUSIC : <span class="footer__music__header__indicator">OFF</span></h4>
+					<p class="footer__music__name-display">Loading...</p>
+
 				</div> 
 
 				<div class="footer__tri-shape">
