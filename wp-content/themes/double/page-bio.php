e87f2d1e7becf680b4899f31106b3323a7ce23f0
Larry Stanfield <vince.omega@gmail.com>
2018-09-15 22:49:54 -0400

    adding new copy to the bio page

--- wp-content/themes/double/page-bio.php
+++ wp-content/themes/double/page-bio.php
@@ -6,44 +6,150 @@ Template Name: Bio Page
 
 <?php get_header(); ?>
 			
-			<div id="page-container" class="container">
+	<div id="page-container" class="container">
 
-                <div class="container__bio">
+        <div class="container__bio">
 
-                    <div class="container__bio__text"> <p> Dolore laborum minim aliquip fugiat aute veniam. Elit qui laboris consectetur ad tempor laboris. Est id ad ullamco proident reprehenderit consequat nulla dolore. Nulla commodo consequat dolor eiusmod in cillum qui minim. Laborum dolor mollit pariatur exercitation quis esse excepteur esse labore aliqua. Cupidatat Lorem cillum consectetur nisi aliquip eu nulla do. Magna qui cupidatat tempor eiusmod. </p> </div>
+            <div class="container__bio__parallax-summer-1961"></div>
+            <h2 class="container__bio__header">Summer 1961:</h2>
+            <div class="container__bio__text"> 
+                <p>
+                    Four young men from West Philadelphia come together for the first time as United Image.  “We were all just crazy about harmony,” Leonard “Butch” Davis explains. “We had a great deal of admiration for each other’s talents. And that made it easy for us to begin to want to work together.”  
+                    (Joe, do you have an old photo of at least you and Butch from the 60’s? These photos will be a treat for your fans and a validation of your long personal relationship and passion for entertaining/singing.)
+                    Over the following years, the group was frequently booked for local shows.  Early gigs for the group included weddings, birthday and anniversary parties, local night clubs and shows in the Pennsylvania, New Jersey and Delaware  areas, as well as the NYC and DC areas. The group continued to modernize the Covers of the Intruders, the Delphonics, and, the Motown staple of soul groups, the Miracles, the Temptations and the Four Tops. 
+                </p> 
+            </div>
+            <h2 class="container__bio__header">1970:</h2>
+            <div class="container__bio__text">
+                <p>
+                    United Image performs at the XXXXXXXXXX, where they are introduced to XXXXXXXX who brought the group to the attention of XXXXXXX at Stax Records.  Having recently broken off from Atlantic Records, Stax Records was seeing incredible success as an independent label, with artists as prominent as Isaac Hayes, Johnnie Taylor and Rufus Thomas.  
+                </p>
+            </div>
+            <h2 class="container__bio__header">1971:</h2>
+            <div class="container__bio__text">
+                <p>
+                    United Image releases “Love’s Creeping up on Me” on Stax Records.  The single received significant airplay in the Philadelphia, NY and DC areas. Quote from you, Joe here, about the release, and about the experience of hearing the first release on the radio.  People love this stuff.  
+                </p>
+            </div>
+            <h2 class="container__bio__header">1972:</h2>
+            <div class="container__bio__text">
+                <p>
+                    After organizational issues began at Stax Records, United Image move d to Branding Iron Records, and recorded “The African Bump.”  The record gained international acclaim.  Stax Records collaborates briefly with Clive Davis and CBS Records, before becoming a part of R&B lore.
+                </p>
+            </div>
+            <h2 class="container__bio__header">1973:</h2>
+            <div class="container__bio__text">
+                <p>
+                    SalSoul Records is founded in NYC by the Cayre Brothers. Their model is focused on the emerging disco scene, and they become known for promoting the genre with the hottest artists for dance, with 12” long-playing singles to support the dance clubs.  The Cayre Brothers begin working with Allan W Felder and Norman Harris, Philadelphia-based songwriters who has already seen significant success with a cadre of artists locally and nationally. Allan Felder and Norman Harris wrote many hits together, from the 1960s (Honey and the Bees, the Delphonics) through the 1980s.  
+                    Norman Harris, Ron Baker and Earl Young, all former members of Philadelphia Internationals MFSB band, create the publishing company, Baker-Harris-Young Productions. Songwriter Allan Felder and former MFSB keyboardist T.G. Conway join  Baker-Harris-Young  at Salsoul.
+                    Double Exposure comes to their attention of the Cayre Brothers through Allan Felder and Norman Harris, DE signs with Salsoul.
+                </p>
+            </div>
+            <h2 class="container__bio__header">1975:</h2>
+            <div class="container__bio__text">
+                <p>
+                    Double Exposure’s first Salsoul release, Ten Percent is penned by T.G. Conway and Allan Felder.  It is recorded in the Sigma Sound Studios in center city Philadelphia.
+                </p>
+            </div>
+            <h2 class="container__bio__header">1976:</h2>
+            <div class="container__bio__text">
+                <p>
+                    “Ten Percent” is release in the Summer of 1976, and is quickly followed by both the long-playing club  version of the hit. Remixed by acclaimed DJ and producer Walter Gibbons, “Ten Percent – Disco Blend” is often referred to as the first 12-inch single made available for purchase to the US public.
+                    The album featured the title track went to No. 2 on the dance/disco charts and reached No. 54 on the Billboard Hot 100, moving within the top 100 for several months.
+                    “Ten Percent” “Every Man” and “My Love is Free” result in many DE bookings and performances across the country and overseas. 
+                </p>
+            </div>
+            <h2 class="container__bio__header">1977:</h2>
+            <div class="container__bio__text">
+                <p>
+                    Double Exposure is back in the Studio, recording its second album for SalSoul Records. Double Exposure appears on Soul Train performing “Ten Percent” and “My Love is Free.” (Larry:  Add link to clip here.)
+                </p>
+            </div>
+            <div class="container__bio__parallax-1978"></div>
+            <h2 class="container__bio__header">1978:</h2>
+            <div class="container__bio__text">
+                <p>
+                    Salsoul releases “Fourplay” DE’s second  album, demonstrating the range of talent of all of the members, and helping the group diversify from the dance club to the nightclub.  Hits include “XXXXXXXX” and “XXXXXX”
+                </p>
+            </div>
+            <h2 class="container__bio__header">1979:</h2>
+            <div class="container__bio_text">
+                <p>
+                    Salsoul releases “Locker Room” the third and final originally-produced album by the original group members.  “I’ve Got the Hots (for Ya)” is released as the club favorite, while “Why Do We Have to Go Our Separate Ways” is the precursor to a generation of steamy ballads to come in R&B for many years.  
+                    Double Exposure is booked to travel to Japan, China, and many countries in Africa. While Disco as a genre is ending in the US, it continues to flourish elsewhere in the world.
+                </p>
+            </div>
+            <div class="container__bio__parallax-1982"></div>
+            <h2 class="container__bio__header">1982:</h2>
+            <div class="container__bio_text">
+                <p>
+                    Double Exposure records a free-standing single, its last on SalSoul Records.  The song “After All This Time” charts just below the Top 50 on the Dance Charts.
+                    Dance music is declining in the US. Overseas performances run at the same rate or higher than local performances and then slow down all together.  
+                </p>
+            </div>
+            <h2 class="container__bio__header">1984:</h2>
+            <div class="container__bio__text">
+                <p>
+                    Salsoul Records closes for business as the Cayre Brothers move on to promote their developing home video business.
+                    1985 through the 1990s
+                    The group disbands and follows their separate – some non-musical – ventures.  
+                    Wallace “Jimmy” Williams becomes a lead singer for The Trammps, a well-known band also part of Baker, Harris and Young Productions.  
+                </p>
+            </div>
+            <h2 class="container__bio__header">2001:</h2>
+            <div class="container__bio__text">
+                <p>
+                    A dance group called M&S uses samples from Double Exposure's "Everyman" in their song called "Salsoul Nugget".
+                </p>
+            </div>
+            <h2 class="container__bio__header">2007:</h2>
+            <div class="container__bio__text">
+                <p>
+                    Double Exposure performance requests never cease.  Additional opportunities to perform together result in a new studio opportunity. The group records “Lady” for _______ record company, at _____________ studios.  The recording is a rare limited and non-commercial release.  It is the last time all of the original members record together.
+                </p>
+            </div>
+            <h2 class="container__bio__header">2011:</h2>
+            <div class="container__bio__text">
+                <p>
+                    Salsoul Records hold a Recognition award event and Double Exposure is the featured artist. DE performs “Ten Percent” and receives the “Salsoul R&B Legends Award”
+                    As you can tell, I’m making this up.  I want to take this all the way up to Charlie resigning and Jimmy’s passing, but this doesn’t appear anywhere on the ‘net so I really need your help.  Even with the Facebook photos, I don’t have a clear timeline.  
+                    Please complete your timeline  and past in a few (maybe just two more relevant photos to this area.
+                    Don’t make it too long…folks want to get to today.
+                </p>
+            </div>
+            <h2 class="container__bio__header">2016:</h2>
+            <div class="container__bio__text">
+                <p>
+                    New member joins…Lorenzo, Kenny? What does he sing? where did he come from? What groups?
+                </p>
+            </div>
+            <h2 class="container__bio__header">2017:</h2>
+            <div class="container__bio__text">
+                <p>
+                    New bass vocalist  (who? Kenny or Lorenzo) joins…singing? Background?  Please provide.) 
+                </p>
+            </div>
+            <div class="container__bio__parallax-2018"></div>
+            <h2 class="container__bio__header">2018:</h2>
+            <div class="container__bio__text">
+                <p>
+                    Double Exposure is BACK!  Back in the studio recording new updated songs for fans everywhere. 
+                    “We are working  hard on updating our new sound, without sacrificing the vocal harmony, powerful horn arrangements and great bass undertones that helped us bring dancers and steppers out on the dance floor,” says  Leonard “Butch” Davis.  
+                    “We have a dynamite production team,” says Joe Harris.
+                </p>
+            </div>
+            <div class="container__bio__parallax-end-card"></div>
+            <div class="container__bio__text">
+                <p>
+                “And Lorenzo Bell and  Kenny Pitt are perfect for keeping us current and relevant. We know people are going to LOVE the new stuff.
+                “It’s been an incredible opportunity from the start,” says Lorenzo Bell or Kenny Pitt.  “To be able to work with and learn from this level of talent and experience was just the type of opportunity I’ve been looking for.”   
+                “Agree 150 percent!” exclaims Kenny or Lorenzo.  “And because this is a true partnership, we’re growing closer together, as we begin this new chapter in the history of this incredible group.  I am honored that I get to be a part of this.”
+                </p>
+            </div>
+            <div class="container__bio__empty-end"></div>
 
-                    <span class="container__bio__parallax-example-one"></span>
-
-                    <div class="container__bio__text"> <p> Dolore laborum minim aliquip fugiat aute veniam. Elit qui laboris consectetur ad tempor laboris. Est id ad ullamco proident reprehenderit consequat nulla dolore. Nulla commodo consequat dolor eiusmod in cillum qui minim. Laborum dolor mollit pariatur exercitation quis esse excepteur esse labore aliqua. Cupidatat Lorem cillum consectetur nisi aliquip eu nulla do. Magna qui cupidatat tempor eiusmod. </p> </div>
-
-                    <span class="container__bio__parallax-example-two"></span>
-
-                    <div class="container__bio__text"> <p> Dolore laborum minim aliquip fugiat aute veniam. Elit qui laboris consectetur ad tempor laboris. Est id ad ullamco proident reprehenderit consequat nulla dolore. Nulla commodo consequat dolor eiusmod in cillum qui minim. Laborum dolor mollit pariatur exercitation quis esse excepteur esse labore aliqua. Cupidatat Lorem cillum consectetur nisi aliquip eu nulla do. Magna qui cupidatat tempor eiusmod. </p> </div>
-
-                    <span class="container__bio__parallax-example-three"></span>
-
-                    <div class="container__bio__text"> <p> Dolore laborum minim aliquip fugiat aute veniam. Elit qui laboris consectetur ad tempor laboris. Est id ad ullamco proident reprehenderit consequat nulla dolore. Nulla commodo consequat dolor eiusmod in cillum qui minim. Laborum dolor mollit pariatur exercitation quis esse excepteur esse labore aliqua. Cupidatat Lorem cillum consectetur nisi aliquip eu nulla do. Magna qui cupidatat tempor eiusmod. </p> </div>
-
-                    <span class="container__bio__parallax-example-two"></span>
-
-                    <div class="container__bio__text"> <p> Dolore laborum minim aliquip fugiat aute veniam. Elit qui laboris consectetur ad tempor laboris. Est id ad ullamco proident reprehenderit consequat nulla dolore. Nulla commodo consequat dolor eiusmod in cillum qui minim. Laborum dolor mollit pariatur exercitation quis esse excepteur esse labore aliqua. Cupidatat Lorem cillum consectetur nisi aliquip eu nulla do. Magna qui cupidatat tempor eiusmod. </p> </div>
-
-                    <span class="container__bio__parallax-example-two"></span>
-
-                    <div class="container__bio__text"> <p> Dolore laborum minim aliquip fugiat aute veniam. Elit qui laboris consectetur ad tempor laboris. Est id ad ullamco proident reprehenderit consequat nulla dolore. Nulla commodo consequat dolor eiusmod in cillum qui minim. Laborum dolor mollit pariatur exercitation quis esse excepteur esse labore aliqua. Cupidatat Lorem cillum consectetur nisi aliquip eu nulla do. Magna qui cupidatat tempor eiusmod. </p> </div>
-
-                    <span class="container__bio__parallax-example-two"></span>
-
-                    <div class="container__bio__text"> <p> Dolore laborum minim aliquip fugiat aute veniam. Elit qui laboris consectetur ad tempor laboris. Est id ad ullamco proident reprehenderit consequat nulla dolore. Nulla commodo consequat dolor eiusmod in cillum qui minim. Laborum dolor mollit pariatur exercitation quis esse excepteur esse labore aliqua. Cupidatat Lorem cillum consectetur nisi aliquip eu nulla do. Magna qui cupidatat tempor eiusmod. </p> </div>
-
-                    <span class="container__bio__parallax-example-two"></span>
-
-                    <div class="container__bio__text"> <p> Dolore laborum minim aliquip fugiat aute veniam. Elit qui laboris consectetur ad tempor laboris. Est id ad ullamco proident reprehenderit consequat nulla dolore. Nulla commodo consequat dolor eiusmod in cillum qui minim. Laborum dolor mollit pariatur exercitation quis esse excepteur esse labore aliqua. Cupidatat Lorem cillum consectetur nisi aliquip eu nulla do. Magna qui cupidatat tempor eiusmod. </p> </div>
-
-                    <span class="container__bio__parallax-example-two"></span>
-
-                </div>
+        </div>
 			
-			</div> <!-- end #content -->
+	</div> <!-- end #content -->
 
 <?php get_footer(); ?>
