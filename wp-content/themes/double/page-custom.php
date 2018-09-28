dd07500b665e624de92ac6d57a1c67b1beff3ed4
Larry Stanfield <vince.omega@gmail.com>
2018-09-28 19:13:19 -0400

    debugging

--- wp-content/themes/double/page-custom.php
+++ wp-content/themes/double/page-custom.php
@@ -11,7 +11,11 @@ Template Name: Custom Page Example
 				<h1 class="container__main-header">THIS IS DOUBLE EXPOSURE</h1>
 			
 				<div class="container__media">
-
+					<div class="container__media__video">
+						<video loop muted autoplay poster="<?php echo get_template_directory_uri(); ?>/img/Double_Exposure_Center_Stage.jpg" class="container__media__video__item">
+							<source src="<?php echo get_template_directory_uri(); ?>/media/video/sample.mp4" type="video/mp4">
+						</video>
+					</div>
 				</div>
 			
 			</div> <!-- end #content -->
