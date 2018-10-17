9e684e258b779da29316e9693b1edd463c185ba9
Larry Stanfield <vince.omega@gmail.com>
2018-10-17 18:18:00 -0400

    includes

--- wp-content/themes/double/includes/modal-contact-us.php
+++ wp-content/themes/double/includes/modal-contact-us.php
@@ -1,4 +1,8 @@
-
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 <?php if( URL[ 'include' ] ){ include '/wp-includes/shortcodes.php'; } ?>
 
 <div class="container__contact-us">
