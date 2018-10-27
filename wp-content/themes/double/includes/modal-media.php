7be88a6249618550a68f6fa68c57cfb1de105bed
Larry Stanfield <vince.omega@gmail.com>
2018-10-27 19:55:39 -0400

    added a loop to go through and add the albumn art images for the songs in media

--- wp-content/themes/double/includes/modal-media.php
+++ wp-content/themes/double/includes/modal-media.php
@@ -1,5 +1,37 @@
+<?php
+
+$dir                    = scandir( 'wp-content/themes/double/media/audio/' );
+$albumArray             = array();
+$albumArrayMirrored     = array();
+$songArray              = array();
+$songArrayMirrored      = array();
+$temp                   = array();
+$i                      = 0;
+foreach( $dir as $dirkey => $dirvalue ){
+    
+    $temp = explode( " - ", $dirvalue );
+    array_push( $albumArray, $temp[ 0 ] );
+    if( isset( $temp[ 1 ] ) ){ 
+        array_push( $songArray, $temp[ 1 ] ); 
+    }
+
+}
+for( $i = count( $albumArray ) - 1; $i > 1; $i-- ){
+   array_push( $albumArrayMirrored, $albumArray[ $i ] );
+}
+for( $i = count( $songArray ) - 1; $i > 1; $i-- ){
+    array_push( $songArrayMirrored, $songArray[ $i ] );
+ }
+//var_dump( $dir );
+//var_dump( $albumArray );
+var_dump( $albumArrayMirrored );
+var_dump( $songArrayMirrored )
+?>
+
+
 <div class="container__media-page">
 
+    <!---
     <div class="container__media-page__video">
         <div class="container__media-page__video__image">
             <a href="wp-content/themes/double/media/video/sample.mp4" class="" media="" media-src="" rel="lightbox[Example Video 50% 70%]" title="Example Video"><img src="https://placeimg.com/320/150/any"></a>
@@ -11,17 +43,22 @@
             Incididunt adipisicing ad sint sit fugiat aute cupidatat sit minim sint.
         </div>
     </div>
+    --->
 
-    <div class="container__media-page__audio">
-        <div class="container__media-page__audio__image">
-            <a href="wp-content/themes/double/media/audio/sample.mp3" class="" media="" media-src="" rel="lightbox[Example Audio 50% 70%]" title="Example Video"><img src="https://placeimg.com/320/150/any"></a>
-            <div class="container__media-page__audio__play">
-                <span><i class="fas fa-play-circle"></i>Play Audio</span>
+    <?php for( $i = 0; $i < count( $songArrayMirrored ) -1; $i++ ){?>
+
+        <div class="container__media-page__audio">
+            <div class="container__media-page__audio__image">
+                <a href="wp-content/themes/double/media/audio/<?php echo urlencode( $songArrayMirrored[ $i ] ); ?>" class="" media="" media-src="" rel="lightbox[Example Audio 50% 70%]" title="Example Video" style="background-image: url('wp-content/themes/double/media/album/<?php echo $albumArrayMirrored[ $i ] . '.png'; ?>');"></a>
+                <div class="container__media-page__audio__play">
+                    <span><i class="fas fa-play-circle"></i>Play Audio</span>
+                </div>
+            </div>
+            <div class="container__media-page__audio__description">
+                Incididunt adipisicing ad sint sit fugiat aute cupidatat sit minim sint.
             </div>
         </div>
-        <div class="container__media-page__audio__description">
-            Incididunt adipisicing ad sint sit fugiat aute cupidatat sit minim sint.
-        </div>
-    </div>
+
+    <?php } ?>
 
 </div>
